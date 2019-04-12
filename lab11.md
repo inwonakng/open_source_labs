d563ff1b30a635e85c88eee047bf5f0bd8cc2a49
inwonakng <inwon.kang04@gmail.com>
2019-04-12 15:11:41 -0400

    yes

--- lab11.md
+++ lab11.md
@@ -5,3 +5,20 @@
 
 #### Checkpoint 2  
 ![this](images/lab11/cp2.png)
+
+#### Checkpoint 3
+
+##### original image:  
+![this](images/lab11/p1.jpeg)
+##### prediction result:  
+![this](images/lab11/cp3_1.png)
+
+##### original image:  
+![this](images/lab11/p2.jpg)
+##### prediction result:
+![this](images/lab11/cp3_2.png)
+
+##### original image:  
+![this](images/lab11/p4.jpg)
+##### prediction result:
+![this](images/lab11/cp3_3.png)
--- lab11/test.py
+++ lab11/test.py
@@ -44,8 +44,8 @@ def plot_value_array(i, predictions_array, true_label):
   thisplot[true_label].set_color('blue')
 
 p1 = ImageOps.invert(Image.open("p1_resized.jpeg").convert("L"))
-p2 = Image.open("p2_resized.jpeg")
-p3 = Image.open("p3_resized.jpg")
+p2 = ImageOps.invert(Image.open("p2_resized.jpg").convert("L"))
+p3 = ImageOps.invert(Image.open("p4_resized.jpg").convert("L"))
 
 fashion_mnist = keras.datasets.fashion_mnist
 
@@ -56,25 +56,25 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 train_images.shape
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
 
 train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
 
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
@@ -92,17 +92,30 @@ predictions = model.predict(test_images)
 num_rows = 5
 num_cols = 3
 num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(9000,9015):
-  plt.subplot(num_rows, 2*num_cols, 2*(i-9000)+1)
-  plot_image(i, predictions, test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*(i-9000)+2)
-  plot_value_array(i, predictions, test_labels)
-plt.show()
+# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+# for i in range(9000,9015):
+#   plt.subplot(num_rows, 2*num_cols, 2*(i-9000)+1)
+#   plot_image(i, predictions, test_labels, test_images)
+#   plt.subplot(num_rows, 2*num_cols, 2*(i-9000)+2)
+#   plot_value_array(i, predictions, test_labels)
+# plt.show()
 
-p1_im = numpy.array(p1)
+p1_im = numpy.array(p3)
 p1_im = (numpy.expand_dims(p1_im,0))
 predictions_single = model.predict(p1_im)
 print(predictions_single)
-plot_value_array(0, predictions_single, test_labels)
-_ = plt.xticks(range(10), class_names, rotation=45)
+
+
+print(numpy.argmax(predictions_single[0]))
+print(class_names[numpy.argmax(predictions_single[0])])
+
+cp3_images = [p3]
+cp3_labels = [4]
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions_single, cp3_labels, cp3_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions_single,  cp3_labels)
+plt.show()
